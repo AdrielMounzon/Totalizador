27e2949b5536f4cf8f8ada66e44694f294ce6b6c
Adriel <adriel.mounzon@ucb.edu.bo>
2023-02-20 18:26:40 -0400

    Mostrar precio por item

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,7 +1,9 @@
-const formulario = document.querySelector("#formularioTotalizador")
-const resultado = document.querySelector("#resultado")
+const formulario = document.querySelector("#formularioTotalizador");
+const resultado = document.querySelector("#resultado");
 
-formulario.addEventListener("submir", event =>
-{
-    event.preventDefault()
-})
+formulario.addEventListener("submit", event => {
+    event.preventDefault();
+    const cantidad = Number.parseInt(document.querySelector("#cantidad").value);
+    const precio = Number.parseFloat(document.querySelector("#precio").value);
+    resultado.innerHTML = cantidad * precio;
+});
